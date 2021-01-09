c3f69acaede75a5d248e16c99fe1925ba7439d92
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-01-09 14:10:08 +1100

    added component to Header.js with text as per illustrated example

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header text-center">
+          <h2>Employee Directory</h2>
+          <p>Click on carrots to filter by heading or use the search box to narrow your results</p>
+        </header>
+    )
+}
+
+export default Header;
