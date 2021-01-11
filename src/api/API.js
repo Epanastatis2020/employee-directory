a718638fb0555e003e612deda00afd92ab276638
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-01-11 21:04:27 +1100

    removed NZ nationality from randomuser.me results so that only australian numbers are shown

--- src/api/API.js
+++ src/api/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   getEmployeeList: function() {
-    return axios.get("https://randomuser.me/api/?results=50&inc=name,phone,nat,email,picture,dob&nat=au,nz");
+    return axios.get("https://randomuser.me/api/?results=50&inc=name,phone,nat,email,picture,dob&nat=au");
   }
 };
