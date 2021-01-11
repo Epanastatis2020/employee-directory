c5b79ecd3efe91b938d3935a2976b28b7593d2c2
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2021-01-11 20:01:18 +1100

    added table elements to tabledata.js

---
+++ src/components/Table/TableData.js
@@ -0,0 +1,31 @@
+import React from "react";
+import '../styles/datatable.css';
+import dateFormat from 'dateformat';
+
+function DataTable(props) {
+    return (
+
+        <table className="table table-striped">
+            <thead>
+                <tr className="tr-header">
+                    {props.columns.map(column => (                        
+                        <th onClick={() => props.handleSort(column.colName)} scope="col" key={column.colName}>{column.colName} <i className="fa fa-fw fa-sort"></i></th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {props.filteredEmployees.map(employee => (
+                    <tr className="tr-stuff" key={employee.email}>
+                        <td><img alt="employee" src={employee.picture.thumbnail}/></td>
+                        <td>{employee.name.first + " " + employee.name.last}</td>
+                        <td>{employee.phone}</td>
+                        <td><a href={employee.email}>{employee.email}</a></td>                        
+                        <td>{dateFormat(`${employee.dob.date}`,"dd/mm/yyyy")}</td>  
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+      );
+}
+
+export default DataTable;
